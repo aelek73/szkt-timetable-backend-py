5369f90352eb3df17ad186343f2f1cb5857a5869
Akos Elek <47024989+akose73@users.noreply.github.com>
2021-03-18 21:09:26 +0100

    Fix slash character in URL

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from gtfs_manager import *
 import flask
+from flask_cors import CORS
 from flask import request, jsonify
 
 app = flask.Flask(__name__)
+CORS(app)
 app.config["DEBUG"] = True
 
 @app.route('/', methods=['GET'])
@@ -13,8 +15,14 @@ def hello_world():
 def api_all(dataArray):
     return jsonify(gtfsToJSON(dataArray))
 
-@app.route('/api/v1/<dataArray>/<key>/<value>', methods=['GET'])
-def agencySearch(dataArray, key, value):
+@app.route('/api/v1/<dataArray>/<path:pars>', methods=['GET'])
+def agencySearch(dataArray, pars):
+    if len(pars.split('/')) >= 3:
+        key = pars.split('/')[0]
+        value = str(pars.split('/')[1]) + '/' + str(pars.split('/')[2])
+    else:
+        key = pars.split('/')[0]
+        value = pars.split('/')[1]
     return jsonify(searchInDict(gtfsToJSON(dataArray), key, value))
 
 app.run()
